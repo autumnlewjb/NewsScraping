47a3ee7c2f5a5a353e5457fcb0dc8a10daf4dcdd
autumnlewjb <49812781+autumnlewjb@users.noreply.github.com>
2020-04-30 14:23:04 +0800

    added new files

---
+++ Url_List.py
@@ -0,0 +1,6 @@
+news_link = [
+'https://www.nst.com.my/news/nation',
+'https://www.nst.com.my/news/crime-courts',
+'https://www.nst.com.my/news/exclusive',
+'https://www.nst.com.my/news/government-public-policy',
+'https://www.nst.com.my/news/politics']
